19a1adf8a8d7a99129bbfe137e5eda0e5ed61f04
MasnyKrak <kuperek121@wp.pl>
2023-04-14 21:26:10 +0000

    v0.1

---
+++ embed.py
@@ -0,0 +1,5 @@
+import discord
+embedVar = discord.Embed(title="Commands", description="", color=0x00ff00)
+embedVar.add_field(name="hello", value="sends a silly photo", inline=False)
+embedVar.add_field(name="valheim on", value="starts valheim server", inline=False)
+embedVar.add_field(name="valheim off", value="stops valheim server", inline=False)
